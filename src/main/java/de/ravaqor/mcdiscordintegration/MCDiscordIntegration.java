b86e6ca59a0a4aedde64cfbe249f45fd1a021610
Alex Wuest <alex.wuest@freenetmail.de>
2026-06-05 10:55:18 +0200

    properties will now be stored in the world file

--- src/main/java/de/ravaqor/mcdiscordintegration/MCDiscordIntegration.java
+++ src/main/java/de/ravaqor/mcdiscordintegration/MCDiscordIntegration.java
@@ -4,12 +4,14 @@ import de.ravaqor.mcdiscordintegration.config.ConfigCommand;
 import de.ravaqor.mcdiscordintegration.config.ModConfig;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.WorldSavePath;
 import org.apache.logging.log4j.core.jmx.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +20,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
 
 public class MCDiscordIntegration implements ModInitializer {
 	public static final String MOD_ID = "mc-discord-integration";
@@ -28,11 +31,21 @@ public class MCDiscordIntegration implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ModConfig.load();
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            Path worldConfig = server.getSavePath(WorldSavePath.ROOT).resolve("mcDiscordIntegration.properties");
+            ModConfig.load(worldConfig);
+        });
+
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
+            ModConfig.save();
+        });
+
         ConfigCommand.register();
 
         ServerMessageEvents.CHAT_MESSAGE.register(MCDiscordIntegration::onChatMessage);
         ServerMessageEvents.GAME_MESSAGE.register(MCDiscordIntegration::onServerMessage);
+
+
     }
 
     private static void onChatMessage(
--- src/main/java/de/ravaqor/mcdiscordintegration/config/ModConfig.java
+++ src/main/java/de/ravaqor/mcdiscordintegration/config/ModConfig.java
@@ -1,7 +1,5 @@
 package de.ravaqor.mcdiscordintegration.config;
 
-import net.fabricmc.loader.api.FabricLoader;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,15 +10,13 @@ import static de.ravaqor.mcdiscordintegration.MCDiscordIntegration.MOD_ID;
 public class ModConfig {
 
     private static final Properties PROPS = new Properties();
+    private static Path currentPath = null;
 
-    private static final Path CONFIG_PATH = FabricLoader.getInstance()
-            .getConfigDir()
-            .resolve("mcDiscordIntegration.properties");
-
-    public static void load() {
-        if (Files.exists(CONFIG_PATH)) {
-            try (var writer = Files.newBufferedWriter(CONFIG_PATH)) {
-                PROPS.store(writer, "mcDiscordIntegration mod-config");
+    public static void load(Path path) {
+        currentPath = path;
+        if (Files.exists(currentPath)) {
+            try (var reader = Files.newBufferedReader(currentPath)) {
+                PROPS.load(reader);
             } catch (IOException e) {
                 System.err.println( MOD_ID + ": Failed to save config: " + e.getMessage());
             }
@@ -30,8 +26,8 @@ public class ModConfig {
     }
 
     public static void save() {
-        try (var writer = Files.newBufferedWriter(CONFIG_PATH)) {
-            PROPS.store(writer, "MyDiscordMod config");
+        try (var writer = Files.newBufferedWriter(currentPath)) {
+            PROPS.store(writer, "MC-Discord-Integration config");
         } catch (IOException e) {
             System.err.println(MOD_ID + " Failed to save config: " + e.getMessage());
         }
